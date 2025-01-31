16e15a297981f06424c7fabdd5a600c290bccd05
Gabriel Lima <26210501+gabrielhuervo@users.noreply.github.com>
2025-01-31 16:57:38 -0300

    04_removeFromArray ✅

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, content) {
+const index = array.indexOf(content)
+
+  if (index > -1) {
+  	array.splice(index, 1)
+		return array
+  }
+  else {
+  	return "Not found."
+  }
 };
 
 // Do not edit below this line
