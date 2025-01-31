e4d83843f277d180a18e94decbc93f20cecc4ad6
Gabriel Lima <26210501+gabrielhuervo@users.noreply.github.com>
2025-01-31 17:05:05 -0300

    05_sumAll ✅

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+	if (start <= 0 || end <= 0) {
+        return "Both numbers must be positive integers.";
+    }
 
+    let sum = 0;
+    for (let i = start; i <= end; i++){
+    	sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
