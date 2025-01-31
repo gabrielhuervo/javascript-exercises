92d9eaae87f5277e59f01f0403e6f9108080b641
Gabriel Lima <26210501+gabrielhuervo@users.noreply.github.com>
2025-01-31 17:19:21 -0300

    06_leapYears ✅

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,17 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+	if(year % 4 === 0){
+		if(year % 100 === 0){
+			if(year % 400 === 0){
+				return true; // Year is divisible by 400, so it's a leap year
+			}else{
+				return false; // Year is divisible by 100 but not by 400, so it's not a leap year
+			}
+		}else{
+			return true; // Year is divisible by 4 but not by 100, so it's a leap year
+		}
+	}else{
+		return false; // Year is not divisible by 4, so it's not a leap year
+	}
 };
 
 // Do not edit below this line
